f8211296398cb246b9b0866658b19b05c5bd061a
dominhjoi <dominjuza@gmail.com>
2026-03-24 21:59:03 +0100

    Add files via upload

---
+++ DiegoGPT.py
@@ -0,0 +1,23 @@
+print("Ahoj ja jsem retardovaný diego")
+print("jak ti mohu pomoci?")
+x = input()
+if x == "rasistickej mod" or x == "negr" or x == "nigga" or x == "Nigga":
+    print("ouhhh hodne tuff")
+    print("jsem rasista ale v teto konverzaci nebudu pokracovat")
+if x == "chcipni diego" or x == "umri diego":
+    print("🥀😭")
+if x == "jebat adama":
+    print("Adam by se měl zabít")
+else:
+    print("fam to jsem do možností nedal😭🥀")
+if x =="nn":
+    print("jak jako ne?")
+    x = input()
+    if x == "proste ne kurva" or x == "naser si diego":
+        print("ok🥀😭👍")
+    elif x == "ne":
+            print("uhhhhh nevím")
+    else:
+            print("fam to jsem do mozností nedal")
+        
+    
